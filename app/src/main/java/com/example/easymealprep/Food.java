bbab1c2e920fac3e5f8015df9bfc25e50570745d
Hyukjoon Yang <hyang348@wisc.edu>
2020-10-26 13:22:30 -0500

    Food.java and SQLConnection.java

---
+++ app/src/main/java/com/example/easymealprep/Food.java
@@ -0,0 +1,143 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
+
+public class Food {
+
+	private Connection conn;
+	private Statement stmt;
+	private CallableStatement cstmt;
+
+	private String userAccount;
+
+	public Food(Connection conn, String userAccount) {
+		this.conn = conn;
+		try {
+			stmt = this.conn.createStatement();
+			cstmt = null;
+			this.userAccount = userAccount;
+
+		} catch (SQLException e) {
+			System.out.println("Error: Food " + e.getMessage());
+		}
+	}
+
+	protected boolean createFood(String foodName, String foodDescription, String picture) {
+		int maxID = 0;
+		try {
+			cstmt = conn.prepareCall("call getFoodID(?);");
+			cstmt.setInt(1, Types.BIGINT);
+			cstmt.executeUpdate();
+			maxID = cstmt.getInt(1);
+			maxID++; // possible problem: No limit
+			byte[] image = imageToByte(picture);
+
+			cstmt = conn.prepareCall("call createFood(?,?,?,?,?,?);");
+			cstmt.setInt(1, maxID);
+			cstmt.setString(2, this.userAccount);
+			cstmt.setString(3, foodName);
+			cstmt.setString(4, foodDescription);
+			cstmt.setBytes(5, image);
+			cstmt.registerOutParameter(6, Types.VARCHAR);
+
+			cstmt.executeUpdate();
+			String status = cstmt.getString(6);
+
+			if (status.equals("Success")) {
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println("Error: createFood " + e.getMessage());
+		}
+		return false;
+	}
+
+	private byte[] imageToByte(String filepath) {
+		byte[] byteArray = null;
+		if (filepath == null) {
+			return byteArray;
+		}
+		ByteArrayOutputStream baos = null;
+		FileInputStream fis = null;
+		try {
+			baos = new ByteArrayOutputStream();
+			fis = new FileInputStream(filepath);
+			byte[] buffer = new byte[1024];
+			int read = 0;
+
+			while ((read = fis.read(buffer, 0, buffer.length)) != -1) {
+				baos.write(buffer, 0, read);
+			}
+			byteArray = baos.toByteArray();
+		} catch (IOException e) {
+			System.out.println("Error: imageToByte " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: imageToByte " + e.getMessage());
+		} finally {
+
+			try {
+				if (baos != null) {
+					baos.close();
+				}
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {
+				System.out.println("Error: imageToByte " + e.getMessage());
+			}
+		}
+		return byteArray;
+	}
+
+	protected boolean deleteFood(String foodName) {
+		String sql = "delete from Food where foodName = \"" + foodName + "\" and userAccount = \"" + this.userAccount
+				+ "\";";
+		try {
+			stmt.execute(sql);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("Error: deleteFood " + e.getMessage());
+		}
+		return false;
+	}
+
+	protected ResultSet searchFood(String foodName) {
+		String sql = "select * from Food where foodName = \"" + foodName + "\"; ";
+		ResultSet rs = null;
+		try {
+			rs = stmt.executeQuery(sql);
+			return rs;
+		} catch (SQLException e) {
+			System.out.println("Error: searchFood " + e.getMessage());
+		}
+		return rs;
+	}
+
+	protected boolean updateFood(int foodID) {
+		return false;
+	}
+
+	protected ResultSet listAllFood() {
+		ResultSet rs = null;
+		try {
+			String sql = "select * from Food;";
+			rs = stmt.executeQuery(sql);
+		} catch (SQLException e) {
+			System.out.println("Error: listFood " + e.getMessage());
+		}
+		return rs;
+	}
+
+	protected ResultSet listUserFood() {
+		ResultSet rs = null;
+		try {
+			String sql = "select * from Food where userAccount = \"" + this.userAccount + "\";";
+			rs = stmt.executeQuery(sql);
+		} catch (SQLException e) {
+			System.out.println("Error: listFood " + e.getMessage());
+		}
+		return rs;
+	}
+}
---
+++ app/src/main/java/com/example/easymealprep/SQLConnection.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLConnection {
+	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private final String URL = "jdbc:mysql://easymeal.cpahsyvhsld2.us-east-2.rds.amazonaws.com/easymeal?autoReconnect=true&useSSL=false"; 
+	private final String USERNAME = "admin";
+	private final String PASSWORD = "administrator";
+	private Connection conn;
+	
+	protected SQLConnection() {
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException e){
+			System.out.println("Error: SQLConnection " + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("Error: SQLConnection " + e.getMessage());
+		}
+	}
+	
+	protected void closeConnection() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println("Error: closeConnection " + e.getMessage());
+		}
+	}
+	
+	protected Connection getConnection() {
+		return conn;
+	}
+}
