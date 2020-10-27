e6a615174546e0505d87278d98dd59b325b3e8d1
Hyukjoon Yang <hyang348@wisc.edu>
2020-10-26 21:01:47 -0500

    update Recipe

--- app/src/main/java/com/example/easymealprep/Recipe.java
+++ app/src/main/java/com/example/easymealprep/Recipe.java
@@ -8,7 +8,7 @@ public class Recipe {
 	private Statement stmt;
 	private CallableStatement cstmt;
 	private String userAccount;
-	
+
 	public Recipe(Connection conn, String userAccount) {
 		this.conn = conn;
 		try {
@@ -19,12 +19,12 @@ public class Recipe {
 			System.out.println("Error: Recipe " + e.getMessage());
 		}
 	}
-	
+
 	protected boolean createRecipe(int foodID, ArrayList<String> instruction) {
 		try {
 			int step = 1;
 			int count = 0;
-			
+
 			if (instruction == null) {
 				return false;
 			}
@@ -35,7 +35,7 @@ public class Recipe {
 				cstmt.setString(3, instruction.get(count));
 				cstmt.setString(4, this.userAccount);
 				cstmt.registerOutParameter(5, Types.VARCHAR);
-				
+
 				cstmt.executeUpdate();
 				String status = cstmt.getString(5);
 				if (!status.equals("Success")) {
@@ -45,18 +45,13 @@ public class Recipe {
 				count++;
 			}
 			return true;
-			
+
 		} catch (SQLException e) {
 			System.out.println("Error: createRecipe " + e.getMessage());
 		}
 		return false;
 	}
-	protected boolean searchUserRecipe() {
-		return false;
-	}
-	protected boolean searchAllRecipe() {
-		return false;
-	}
+
 	protected boolean deleteRecipe(int foodID) {
 		try {
 			cstmt = conn.prepareCall("call deleteRecipe (?,?);");
@@ -71,9 +66,55 @@ public class Recipe {
 		}
 		return false;
 	}
-	protected boolean updateRecipe() {
+
+	protected boolean updateRecipe(int foodID, ArrayList<String> instruction) {
+		try {
+			int step = 1;
+			int count = 0;
+
+			if (instruction == null) {
+				return false;
+			}
+
+			ResultSet rs = this.searchOneRecipe(foodID);
+			int original = 0;
+			while (rs.next()) {
+				original++;
+			}
+
+			int diff = original - instruction.size();
+
+			while (count < instruction.size()) {
+				cstmt = conn.prepareCall("call updateRecipe(?,?,?,?);");
+				cstmt.setInt(1, foodID);
+				cstmt.setInt(2, step);
+				cstmt.setString(3, instruction.get(count));
+				cstmt.setString(4, this.userAccount);
+
+				int result = cstmt.executeUpdate();
+				if (result == 0) {
+					System.out.println("result1: " + result);
+					return false;
+				}
+				step++;
+				count++;
+			}
+			if (diff > 0) {
+				String sql = "delete from Recipe where foodID = " + foodID + " and step >= " + step + ";";
+				int result = stmt.executeUpdate(sql);
+				if (result == 0) {
+					System.out.println("result2: " + result);
+					return false;
+				}
+			}
+			return true;
+
+		} catch (SQLException e) {
+			System.out.println("Error: createRecipe " + e.getMessage());
+		}
 		return false;
 	}
+
 	protected ResultSet searchOneRecipe(int foodID) {
 		try {
 			String sql = "select * from Recipe where foodID = " + foodID + " order by step ASC;";
