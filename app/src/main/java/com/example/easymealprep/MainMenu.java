c4815a71096749e7f291ea51c51e5e5ca5845bd2
Jettypile <36287441+Jettypile@users.noreply.github.com>
2020-10-29 17:37:17 -0500

    Add files via upload

--- app/src/main/java/com/example/easymealprep/MainMenu.java
+++ app/src/main/java/com/example/easymealprep/MainMenu.java
@@ -2,13 +2,50 @@ package com.example.easymealprep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Button quit;
+    NavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        quit = (Button) findViewById(R.id.Quit);
+            nav = (NavigationView) findViewById(R.id.bottomNav);
+        nav.setNavigationItemSelectedListener(this);
+
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2login = new Intent(MainMenu.this, MainActivity.class);
+                startActivity(intent2login);
+            }
+        });
+
     }
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_search) {
+
+        } else if (id == R.id.nav_favorites) {
+
+
+
+        }
+        return true;
+    }
+
+
 }
