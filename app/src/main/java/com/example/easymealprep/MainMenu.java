cfb24cdeb606c5d980a8e638814ea38b706853c2
dhondup2 <tsewang-dhon@live.com>
2020-10-29 22:27:25 -0500

    added fragment files for bottom nav

--- app/src/main/java/com/example/easymealprep/MainMenu.java
+++ app/src/main/java/com/example/easymealprep/MainMenu.java
@@ -1,6 +1,7 @@
 package com.example.easymealprep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,41 +13,45 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainMenu extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     Button quit;
-    BottomNavigationView nav;
+    private BottomNavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        quit = (Button) findViewById(R.id.Quit);
+//        quit = (Button) findViewById(R.id.Quit);
         nav = (BottomNavigationView) findViewById(R.id.bottomNav);
         nav.setOnNavigationItemSelectedListener(this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
 
-        quit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Statics.connection.closeConnection();
-                Intent intent2login = new Intent(MainMenu.this, MainActivity.class);
-                startActivity(intent2login);
-            }
-        });
+
+//        quit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Statics.connection.closeConnection();
+//                Intent intent2login = new Intent(MainMenu.this, MainActivity.class);
+//                startActivity(intent2login);
+//            }
+//        });
 
     }
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            fragment = new HomeFragment();
         } else if (id == R.id.nav_search) {
-
+            fragment = new SearchFragment();
         } else if (id == R.id.nav_add_recipe) {
-
+            fragment = new AddRecipeFragment();
         } else if (id == R.id.nav_favorites) {
-
+            fragment = new FavoritesFragment();
         } else if (id == R.id.nav_setting) {
-            
+            fragment = new SettingFragment();
         }
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
         return true;
     }
 
