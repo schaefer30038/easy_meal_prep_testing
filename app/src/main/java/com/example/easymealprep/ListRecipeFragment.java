35afd2652d5adacef8a7951a0ef43a968f8b5b5c
dhondup2 <tsewang-dhon@live.com>
2020-11-11 17:51:33 -0600

    list all recipe updated

--- app/src/main/java/com/example/easymealprep/ListRecipeFragment.java
+++ app/src/main/java/com/example/easymealprep/ListRecipeFragment.java
@@ -1,15 +1,21 @@
 package com.example.easymealprep;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -39,5 +45,67 @@ public class ListRecipeFragment extends Fragment {
 
 
         return inputFragmentView;
+    }
+    public class ListAllFoodAsync extends AsyncTask<Void,Void,Void> {
+        Food food;
+        ResultSet resultSet;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            food = new Food(Statics.connection.getConnection(), Statics.currUserAccount);
+            resultSet = food.listAllFood();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            ArrayList<String> list = new ArrayList<>();
+            if (resultSet != null) {
+                System.out.println("ASDasd");
+                try {
+                    while (resultSet.next()) {
+                        int foodID = resultSet.getInt("foodID");
+                        String foodName = resultSet.getString("foodName");
+                        String foodDescription = resultSet.getString("foodDescription");
+                        list.add(foodName);
+                        byte [] foodPic = resultSet.getBytes("foodPic");
+                        Object[] array = new Object[4];
+                        array[0] = foodID;
+                        array[1] = foodName;
+                        array[2] = foodDescription;
+                        array[3] = foodPic;
+                        arrayLists.add(array);
+                        System.out.println(foodName);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, list);
+
+            listView.setAdapter(arrayAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Statics.currFood[0] = arrayLists.get(position)[0];
+                    Statics.currFood[1] = arrayLists.get(position)[1];
+                    Statics.currFood[2] = arrayLists.get(position)[2];
+                    Statics.currFood[3] = arrayLists.get(position)[3];
+                    Fragment newFragment = new FoodFragment();
+                    // consider using Java coding conventions (upper first char class names!!!)
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                    // Replace whatever is in the fragment_container view with this fragment,
+                    // and add the transaction to the back stack
+                    transaction.replace(R.id.fragment_container, newFragment);
+                    transaction.addToBackStack(null);
+
+                    // Commit the transaction
+                    transaction.commit();
+                }
+            });
+        }
     }
 }
